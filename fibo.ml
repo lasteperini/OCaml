e652c2757a734faffc3a54c4aa7115d78a26ea63
Lavinia Fabrello <lavinia.fabrello@gmail.com>
2019-10-27 18:15:34 +0100

    Delete fibo.ml

--- fibo.ml
+++
@@ -1,8 +0,0 @@
-open List;;
-let rec fibo(n) = if n<=1 then n else fibo(n-1) + fibo(n-2);;
-let main() =
-	let ins = [5; 7; 15; 25; 30] in
-	for i=0 to List.length ins -1 do
-		print_endline("fibo("^string_of_int(nth ins i)^") :- "^string_of_int(fibo(nth ins i)));
-	done;;
-main();;
